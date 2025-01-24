084a8d05f3f9eb8b6e55e1386c803f0cc6c3d08c
Huer <97923571+HuerRome@users.noreply.github.com>
2025-01-24 18:35:14 -0300

    Firts commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let menu=document.querySelector(".menu-active");
+let button= document.querySelector(".menu");
+
+button.addEventListener('click',()=>{
+    menu.classList.toggle("active");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
